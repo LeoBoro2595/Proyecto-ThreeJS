6cb4c44d2d0ea789480191f3c1bae7b66662c0f5
Leonel <leoboro1234@gmail.com>
2023-06-15 01:14:09 -0300

    Almost done popup UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
 import * as THREE from "three";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
-import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-
-let scene, camera, renderer, cube, wall, controls;
+let scene, camera, renderer, cube, wall, controls, popup, closePopup;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
+    this.popupRef = React.createRef();
     this.animate = this.animate.bind(this);
+    this.onCanvasClick = this.onCanvasClick.bind(this);
+    this.closePopup = this.closePopup.bind(this);
+  }
+
+  closePopup() {
+    this.popupRef.current.style.display = "none";
+  }
+
+  componentDidMount() {
+    this.init();
+    this.animate();
+    this.canvasRef.current.addEventListener("click", this.onCanvasClick);
+  }
+
+  componentWillUnmount() {
+    this.canvasRef.current.removeEventListener("click", this.onCanvasClick);
   }
 
   init() {
@@ -22,14 +37,19 @@ class App extends Component {
     scene.background = new THREE.Color(0x000000);
 
     // Add camera
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight);
+    camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
     camera.position.z = 20;
     camera.position.y = 5;
 
     // Screen renderer
     renderer = new THREE.WebGLRenderer({ canvas: this.canvasRef.current });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    window.addEventListener('resize', function() {
+    window.addEventListener("resize", function () {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
@@ -40,63 +60,105 @@ class App extends Component {
     scene.add(grid);
 
 
-    //Add Point Light    
-    var light = new THREE.PointLight( 0xffffff, 0.4);
+
+
+
+
+    
+    // Add Point Light
+    var light = new THREE.AmbientLight(0xffffff, 0.4);
     scene.add(light);
     light.position.set(1, 5, 8);
 
 
-    // Add GLTF Model
-    var loader = new GLTFLoader();
 
-    var obj;
-    loader.load("../models/camionetatest.gltf", function (gltf) {
-      obj = gltf.scene;
-      scene.add(gltf.scene);
-    })
+
+
 
     // Add geometry
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshStandardMaterial({ color: 0xff0000, wireframe: false, });
+    var material = new THREE.MeshStandardMaterial({
+      color: 0xff0000,
+      wireframe: false,
+    });
     cube = new THREE.Mesh(geometry, material);
-    
     scene.add(cube);
     cube.position.y = 0.5;
 
-
     // Add wall
     var wallgeometry = new THREE.BoxGeometry(10, 5, 0.1);
-    var wallmaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, wireframe: false, });
+    var wallmaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, wireframe: false });
     wall = new THREE.Mesh(wallgeometry, wallmaterial);
     scene.add(wall);
     wall.position.z = -5;
-    wall.position.y = 2.5;       
-
+    wall.position.y = 2.5;
 
     // OrbitControls
     controls = new OrbitControls(camera, renderer.domElement);
 
-    // controls = new FirstPersonControls (camera, renderer.domElement);
 
 
-    
-    return renderer.domElement;
+    // Add popup geometry
+    var geometrypopup = new THREE.RingGeometry(1, 2, 32);
+    var materialpopup = new THREE.MeshPhongMaterial({ color: 0xffffff, wireframe: false, emissive: 0xffffff, shininess: 100 });
+    popup = new THREE.Mesh(geometrypopup, materialpopup);
+    popup.castShadow = false;
+    scene.add(popup);
+    popup.position.y = 0.5;
+    popup.position.x = 5;
+
+    popup.rotation.x = 30;
   }
 
-  // Renderizado de pantalla
   animate() {
     requestAnimationFrame(this.animate);
     renderer.render(scene, camera);
-    controls.update(); // Actualiza los controles de la cámara
+    controls.update();
   }
 
-  componentDidMount() {
-    const renderElement = this.init();
-    this.animate();
+  onCanvasClick(event) {
+    const rect = this.canvasRef.current.getBoundingClientRect();
+    const mouse = {
+      x: ((event.clientX - rect.left) / rect.width) * 2 - 1,
+      y: -((event.clientY - rect.top) / rect.height) * 2 + 1,
+    };
+
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObject(popup);
+
+    if (intersects.length > 0) {
+      this.showPopup();
+    }
   }
+
+  showPopup() {
+    const popup = this.popupRef.current;
+    popup.style.display = "block";
+  }
+
 
   render() {
-    return <canvas ref={this.canvasRef} className="App" />;
+    return (
+      <div>
+        <canvas ref={this.canvasRef} className="App" />
+        <div id="popup" className="popup" ref={this.popupRef} style={{ display: "none", position: "absolute", top: 0, left: 0 }}>
+        <i className="fa-solid fa-xmark" id="crossClose" onClick={this.closePopup}></i>
+
+        <video src="https://www.youtube.com/watch?v=85qbNFHshdY&list=RDGMEM2VCIgaiSqOfVzBAjPJm-ag&start_radio=1" id="videoDiv" controls></video>
+
+          <p id="UItext">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus voluptas,
+          quisquam nam deleniti voluptatem explicabo exercitationem quas laudantium fuga accusamus officia architecto eligendi optio repellat labore hic inventore.
+          Distinctio, labore.
+          </p>
+          
+          <p className="creditosProyecto"></p>
+
+        </div>
+      </div>
+    );
   }
 }
 
